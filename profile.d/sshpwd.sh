fbb507b2228daac5a5d9dd5aec27c110616b0b4d
max <max@pi5>
2025-11-07 14:40:51 +0100

    Initial commit

---
+++ profile.d/sshpwd.sh
@@ -0,0 +1,8 @@
+export TEXTDOMAIN=pam_chksshpwd
+
+if [ -e /run/sshwarn ] ; then
+    echo
+	echo $"SSH is enabled and the default password for the 'pi' user has not been changed."
+	echo $"This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password."
+	echo
+fi
---
+++ profile.d/wifi-check.sh
@@ -0,0 +1,22 @@
+(
+        export TEXTDOMAIN=wifi-check
+
+        . gettext.sh
+
+        if [ ! -x /usr/sbin/rfkill ] || [ ! -r /dev/rfkill ]; then
+                exit 0
+        fi
+
+        if ! /usr/sbin/rfkill list wifi | grep -q "Soft blocked: yes" ; then
+                exit 0
+        fi
+
+        if grep -s -q 'cfg80211\.ieee80211_regdom=' /boot/firmware/cmdline.txt; then
+                exit 0
+        fi
+
+        echo
+        /usr/bin/gettext -s "Wi-Fi is currently blocked by rfkill."
+        /usr/bin/gettext -s "Use raspi-config to set the country before use."
+        echo
+)
